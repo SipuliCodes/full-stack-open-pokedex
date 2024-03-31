94d31a53ccb02b2b778ac2338c4b6b8cb4c6f453
SipuliCodes <vepsalainensisu@gmail.com>
2024-03-31 20:53:16 +0300

    11c valmis

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (_req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
   res.status(200).send('ok')
 })
 
